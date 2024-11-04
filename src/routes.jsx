700dc02ff044b02628cced305a58171415f80b96
Wallace Santana <138271229+wallacemt@users.noreply.github.com>
2024-11-04 20:04:57 -0300

    upp routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,16 +1,16 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginScreen from './screens/auth/login';
 import CadastroScreen from './screens/auth/cadastro';
 
 const AppRoutes = () => {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename="/">
             <Routes>
                 <Route path="/" element={<LoginScreen />} />
-                <Route path='/register' element={<CadastroScreen />} />
+                <Route path="/register" element={<CadastroScreen />} />
             </Routes>
         </BrowserRouter>
-    )
-}
+    );
+};
 
-export default AppRoutes
+export default AppRoutes;
