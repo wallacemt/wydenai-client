053c3768d005293d1f9e78ddd4780d1f9ae32d5c
Wallace Santana <138271229+wallacemt@users.noreply.github.com>
2024-11-04 20:15:41 -0300

    up

--- src/components/bannerLateral/index.jsx
+++ src/components/bannerLateral/index.jsx
@@ -3,9 +3,10 @@ import React from "react";
 const BannerLateral = () => (
     <div className="w-[70vw] h-screen bg-[#AB227D]">
         <div
-            className="bg-[url('../../../public/ilustration.png')] bg-no-repeat bg-center bg-cover h-[100%] w-[100%] "
+            className="bg-[url('/ilustration.png')] bg-no-repeat bg-center bg-cover h-[100%] w-[100%] "
         ></div>
     </div>
 );
+
 
 export default BannerLateral;
